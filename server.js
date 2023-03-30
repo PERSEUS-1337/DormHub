1a047c1b3b56f68170895100c2d2f2f9973411ba
PERSEUS-1337 <abramillano@up.edu.ph>
2023-03-31 01:36:29 +0800

    add: Initital MERN Stack

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const routes = require('./routes/api');
+require('dotenv').config();
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+// THIS PART IS STILL TODO FOR [DB]
+// Connect to the database
+// mongoose
+//   .connect(process.env.DB, { useNewUrlParser: true })
+//   .then(() => console.log(`Database connected successfully`))
+//   .catch((err) => console.log(err));
+
+// Since mongoose's Promise is deprecated, we override it with Node's Promise
+// mongoose.Promise = global.Promise;
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+app.use(bodyParser.json());
+
+app.use('/api', routes);
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  next();
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
