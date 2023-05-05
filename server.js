455d024c1f85338383d64606647c918cdbc20247
Aron Resty Ramillano <93975273+PERSEUS-1337@users.noreply.github.com>
2023-05-05 16:54:01 +0800

    Merge feat/accommodation-be back to develop (#1)

--- server.js
+++ server.js
@@ -13,10 +13,10 @@ require('dotenv').config();
 
 const express = require('express');
 const mongoose = require('mongoose');
-// const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 
 // Import Routes
-const accomodationRouter = require('./routes/accomodationRouter');
+const accommodationRouter = require('./routes/accommodationRouter');
 const authRouter = require('./routes/authRouter');
 
 // Express application
@@ -28,9 +28,11 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
-// app.use(bodyParser.json());
+
+app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
+
 app.use((err, req, res, next) => {
   console.log(err);
   next();
@@ -40,13 +42,15 @@ app.use((err, req, res, next) => {
 app.get('/api/v1', (req, res) => {
   res.json({ msg: 'This is the API route' });
 });
-app.use('/api/v1/accomodations', accomodationRouter);
+app.use('/api/v1/accommodation', accommodationRouter);
 app.use('/api/v1/auth', authRouter);
 app.get('*', (req, res) => {
     res.json({ msg: 'Welcome to the Backend. All other routes not declared in the routes folder will be routed automatically to this message' });
 });
 
 // Connect to the database and listen for requests
+console.log('Awakening the server...');
+
 mongoose.connect(process.env.MONGO_URI)
   .then(() =>{
     app.listen(process.env.PORT, () => {
