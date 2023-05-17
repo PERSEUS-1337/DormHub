5c6f8325c9e2646638a65adf58cfae4e3d0199f9
almira <aaalmira@up.edu.ph>
2023-05-18 03:29:37 +0800

    updated API calls, added owner/user choice

--- client/src/pages/signup.jsx
+++ client/src/pages/signup.jsx
@@ -8,7 +8,12 @@ const Signup = () => {
   const [lName, setlName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const navigateTo= useNavigate();
+  const [userType, setUserType] = useState('user'); 
+  const navigateTo = useNavigate();
+
+  const handleLoginClick = () => {
+    navigateTo("/login");
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -16,66 +21,90 @@ const Signup = () => {
     const formData = { fName, lName, email, password };
 
     try {
-      const res = await fetch('/api/signup', {
+      const res = await fetch(`/api/v1/register/${userType}`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
         },
         body: JSON.stringify(formData)
       });
-      console.log(res);
-      // handle success
+
+      if (res.ok) {
+        navigateTo('/');
+      } else {
+        console.error('Registration failed');
+      }
     } catch (err) {
       console.error(err);
-      // handle error
     }
   };
 
   return (
     <>
-    <div className='signup-container'>
-      <h2>Create an account</h2>
-      <Form onSubmit={handleSubmit}>
-        <Form.Group controlId="formFirstName">
-          <Form.Label>First Name</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Enter first name"
-            value={fName}
-            onChange={(e) => setfName(e.target.value)}
-          />
-        </Form.Group>
-        <Form.Group controlId="formLastName">
-          <Form.Label>Last Name</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Enter last name"
-            value={lName}
-            onChange={(e) => setlName(e.target.value)}
-          />
-        </Form.Group>
-        <Form.Group controlId="formEmail">
-          <Form.Label>Email</Form.Label>
-          <Form.Control
-            type="email"
-            placeholder="Enter email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </Form.Group>
-        <Form.Group controlId="formPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="Enter password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </Form.Group>
-        <Button type="submit">Create Account</Button>
-        {/* <Button onClick={navigateTo("/login")}>Go to Login</Button>   */}
-      </Form>
-    </div>
+      <div className='signup-container'>
+        <h2>Create an account</h2>
+        <Form onSubmit={handleSubmit}>
+          <Form.Group controlId="formFirstName">
+            <Form.Label>First Name</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter first name"
+              value={fName}
+              onChange={(e) => setfName(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group controlId="formLastName">
+            <Form.Label>Last Name</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter last name"
+              value={lName}
+              onChange={(e) => setlName(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group controlId="formEmail">
+            <Form.Label>Email</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group controlId="formPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Enter password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group controlId="formUserType">
+            <Form.Label>User Type</Form.Label>
+            <Form.Check
+              inline
+              type="radio"
+              label="User"
+              name="userType"
+              value="user"
+              checked={userType === 'user'}
+              onChange={() => setUserType('user')}
+            />
+            <Form.Check
+              inline
+              type="radio"
+              label="Owner"
+              name="userType"
+              value="owner"
+              checked={userType === 'owner'}
+              onChange={() => setUserType('owner')}
+            />
+          </Form.Group>
+          <Button type="submit">Create Account</Button>
+          <Button onClick={handleLoginClick}>Go to Login</Button>
+        </Form>
+      </div>
     </>
   );
 };
