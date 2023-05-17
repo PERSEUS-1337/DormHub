be75cab741ca50420a7867450703cf1b51205eae
PERSEUS-1337 <abramillano@up.edu.ph>
2023-05-18 01:54:17 +0800

    chore: removed unused funcs

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -5,22 +5,17 @@ const router = express.Router();
 const {registerUser, loginUser, getAllUsers, getUserData} = require('../controllers/userController');
 const {registerOwner, loginOwner, getAllOwners} = require('../controllers/ownerController');
 
-
+// SAMPLE: Get data to test
 router.get('/users', getAllUsers);
 router.get('/owners', getAllOwners);
 
-// // POST: Register a user
+// POST: Register a user
 router.post('/register/user', registerUser);
 
-// // POST: Register an owner
+// POST: Register an owner
 router.post('/register/owner', registerOwner);
 
 router.post('/login/user', loginUser);
 router.post('/login/owner', loginOwner);
-
-// UPDATE an accommodation
-router.patch('/update-user', (req, res, next) => {
-    res.json({msg: 'UPDATE a user'})
-});
 
 module.exports = router; 
