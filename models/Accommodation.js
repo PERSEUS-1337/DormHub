88e361a78f66d5b466bfa04d939c9ecd43133929
jelacson <jelacson@up.edu.ph>
2023-05-18 01:15:42 +0800

    fix: added pics for Accommodation model

--- models/Accommodation.js
+++ models/Accommodation.js
@@ -3,98 +3,100 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const accomodationSchema = new Schema({
-  _id: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true
-  },
-  name: {
-    type: String,
-    required: true
-  },
-  // add desc --
-  desc: {
-    type: String,
-    required: true
-  },
-  price: {
-    type: Number,
-    required: true
-  },
-  location: {
-    type: String,
-    required: true
-  },
-  // fixed type
-  type: {
-    type: [String],
-    enum: ["apartment", "condominium", "dormitory", "transient", "hotel", "hostel", "bedspace"],
-    required: true
-  },
-  // removed rating (FE/BE will average the values from review attribute)
-  // rating: {
-  //   rate: {
-  //     type: Number, 
-  //     enum: [1, 2, 3, 4, 5],
-  //     required: true
-  //   },
-  //   comment: {
-  //     type: String
-  //   }
-  // },
-  amenity: {
-    type: [String],
-    required: true
-  },
-  owner: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true
-  },
-  // removed user
-  // user: {
-  //   type: [mongoose.Schema.Types.ObjectId],
-  //   required: true
-  // },
-  // add archived --
-  archived: {
-    type: Boolean,
-    required: true,
-  },
-  // fixed review
-  review: {
-    type: [
-      {
-        rating: {
-          type: Number,
-          enum: [1, 2, 3, 4, 5],
-          required: true
-        },
-        user: {
-          type: mongoose.Schema.Types.ObjectId,
-          required: true
-        },
-        detail: {
-          type: String,
-          required: true
-        },
-        liked: {
-          type: Boolean
-        }
-      }
-    ],
-    // required: true
-  },
-  // removed report --
-  // report: {
-  //   type: [
-  //     {
-  //       concern: {
-  //         type: String,
-  //         required: true
-  //       }
-  //     }
-  //   ],
-  //   required: true
-  // }
+    _id: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    // add desc --
+    desc: {
+        type: String,
+        required: true
+    },
+    // add pics -- 
+    pics: {
+        type: [String],
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    location: {
+        type: String,
+        required: true
+    },
+    // fixed type
+    type: {
+        type: [String],
+        enum: ["apartment", "condominium", "dormitory", "transient", "hotel", "hostel", "bedspace"],
+        required: true
+    },
+    // removed rating (FE/BE will average the values from review attribute)
+    // rating: {
+    //   rate: {
+    //     type: Number, 
+    //     enum: [1, 2, 3, 4, 5],
+    //     required: true
+    //   },
+    //   comment: {
+    //     type: String
+    //   }
+    // },
+    amenity: {
+        type: [String],
+        required: true
+    },
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true
+    },
+    // removed user
+    // user: {
+    //   type: [mongoose.Schema.Types.ObjectId],
+    //   required: true
+    // },
+    // add archived --
+    archived: {
+        type: Boolean,
+        required: true,
+    },
+    // fixed review
+    review: {
+        type: [{
+            rating: {
+                type: Number,
+                enum: [1, 2, 3, 4, 5],
+                required: true
+            },
+            user: {
+                type: mongoose.Schema.Types.ObjectId,
+                required: true
+            },
+            detail: {
+                type: String,
+                required: true
+            },
+            liked: {
+                type: Boolean
+            }
+        }],
+        // required: true
+    },
+    // removed report --
+    // report: {
+    //   type: [
+    //     {
+    //       concern: {
+    //         type: String,
+    //         required: true
+    //       }
+    //     }
+    //   ],
+    //   required: true
+    // }
 });
 
 const Accomodation = mongoose.model('Accomodation', accomodationSchema);
