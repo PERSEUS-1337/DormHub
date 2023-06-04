3a8a2a6b560d7912a7583e61970423947eddcb73
Jemuel Juatco <jjuatco@up.edu.ph>
2023-06-05 00:04:11 +0800

    fix: added view all accommodations

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -1,14 +1,18 @@
 import React, { useState } from 'react'
-import { Container, Col, Row, Form, Button, Alert} from 'react-bootstrap';
+import { Container, Col, Row, Form, Button, Alert, Spinner, Pagination} from 'react-bootstrap';
 import LodgingTileItem from './LodgingTileItem';
 
 const SearchBar = ({ data }) => {
+    const [viewAll, setViewAll] = useState(false)
+    const [isLoading, setIsLoading] = useState(data);
     const [filteredData, setFilteredData] = useState([])
     const [wordEntered, setWordEntered] = useState("")
     const [show, setShow] = useState(false)
+    const [currentPage, setCurrentPage] = useState(1)
+    const itemsPerPage = 3
 
     const handleFilter = (e) => {
-        try{
+        try {
 
             const searchedWord = e.target.value
             setWordEntered(searchedWord)
@@ -22,9 +26,10 @@ const SearchBar = ({ data }) => {
             } else {
                 setFilteredData(newFilter)
             }
+            setCurrentPage(1)
 
 
-        }catch (error) {
+        } catch (error) {
             console.log(error)
             setShow(true)
 
@@ -39,70 +44,129 @@ const SearchBar = ({ data }) => {
         setShow(false)
         clearInput()
     }
+    
+    const handleViewAll = () => {
+        setFilteredData(data)
+        setCurrentPage(1)
+    }
 
-    const returnResults = () => {
-        return (<>
-            {
-                filteredData.length != 0 && (
-                    <Container className='rounded-3 mt-4' style={{background: "#ffffff"}}>
-                        {filteredData.slice(0, 10).map((value, key) => {
-                            return (
-                                <LodgingTileItem key={value.id} data={value} />
-                                // <p key={key._id}>{value.name}</p>
-                           
-                            )
-                        })
-                        }
-                    </Container>
-        )
+    const indexLastItem = currentPage * itemsPerPage
+    const indexFirstItem = indexLastItem - itemsPerPage
+    const currentItems = filteredData.slice(indexFirstItem, indexLastItem)
+
+    const totalPages = Math.ceil(filteredData.length / itemsPerPage)
+    
+    const handlePageChange = (pageNumber) => {
+        setCurrentPage(pageNumber)
+    }
+
+    const handlePreviousPage = () => {
+        if (currentPage > 1) {
+            setCurrentPage(currentPage - 1)
         }
-        </>)
     }
 
-    if (show) {
-        return (
-            <Alert variant="danger" onClose={() => dismissAlert()} dismissible className='mt-3 mx-3' style={{caretColor: "transparent"}}>
-                <Alert.Heading>Oh snap!</Alert.Heading>
-                <p>
-                    The system has malfunctioned and has notified the developers. Please wait a moment :)
-                </p>
-            </Alert>
-        )
+    const handleNextPage = () => {
+        if (currentPage < totalPages) {
+            setCurrentPage(currentPage + 1)
+        }
     }
+
+    const handleSearch = (word) => {
+        
+        const newFilter = data.filter((value) => {
+                return value.name.toLowerCase().includes(word.toLowerCase())
+        })
+        setFilteredData(newFilter)
+        setCurrentPage(1)
+
+    }
+    // const returnResults = () => {
+    //     return (<>
+    //         {
+    //             filteredData.length != 0 && (
+    //                 <Container className='rounded-3 mt-4' style={{ background: "#ffffff" }}>
+    //                     {filteredData.slice(0, 10).map((value, key) => {
+    //                         return (
+    //                             <LodgingTileItem key={value.id} data={value} />
+    //                             // <p key={key._id}>{value.name}</p>
+                        
+    //                         )
+    //                     })
+    //                     }
+    //                 </Container>
+    //             )
+    //         }
+    //     </>)
+    // }
+
+        if (show) {
+            return (
+                <Alert variant="danger" onClose={() => dismissAlert()} dismissible className='mt-3 mx-3' style={{ caretColor: "transparent" }}>
+                    <Alert.Heading>Oh snap!</Alert.Heading>
+                    <p>
+                        The system has malfunctioned and has notified the developers. Please wait a moment :)
+                    </p>
+                </Alert>
+            )
+        }
         return (
             <Container className='mt-4'>
                 <Row>
-                    <Col lg={12} className='px-5'>
+                    <Col className='px-5'>
                         <Form>
-                            <Form.Group controlId="filterAccomms"  className='d-flex align-items-center'>
-                                <Form.Control type="search" placeholder="Search for an accommodation..." className='m-3' onChange={handleFilter} value={wordEntered} />
-                                {/* <Button onClick={() => returnResults()} id="searchbtn" className ="rounded-1" variant="secondary">SEARCH</Button> */}
+                            <Form.Group controlId="filterAccomms" className='d-flex align-items-center mx-2'>
+                                <Form.Control type="search" placeholder="Search for an accommodation..." className='m-4' onChange={handleFilter} onClick={handleFilter} value={wordEntered} />
+                                <Button className="rounded-1 mx-2" variant="secondary" onClick={e => handleSearch(e.target.value)}>Search</Button>
+                                <Button className="rounded-1 mx-2 text-nowrap" variant="secondary" onClick={handleViewAll}>View All</Button>
                             </Form.Group>
                         </Form>
                     </Col>
         
                 </Row>
-                {/* <Row>
-                    <Col lg={12} className='px-5'>
-                        <span className='m-5'>Sort by</span>
-                        
-                    </Col>
-                </Row> */}
-                {/* Initial state of results when user inputs is the LodgingTileList, will change later */}
                 {filteredData.length != 0 && (
-                    <Container className='rounded-3 mt-4' style={{background: "#ffffff"}}>
-                        {filteredData.slice(0, 10).map((value, key) => {
+                    <Container className='rounded-3 mt-5' style={{ background: "#ffffff" }}>
+                        <Row className='d-flex align-items-center ms-auto'>
+                            <Col><h4>ACCOMMODATIONS: </h4></Col>
+                            <Col className='d-flex justify-content-end'><Button variant='secondary'>Generate PDF</Button></Col>
+                        </Row>
+                        
+                        <br />
+                        {currentItems.map((value, key) => {
                             return (
-                                <LodgingTileItem key={value.id} data={value} />
-                                // <p key={key._id}>{value.name}</p>
+                                <LodgingTileItem key={key} data={value} />
                            
                             )
                         })
                         }
+                        <Pagination className='d-flex justify-content-center'>
+                            <Pagination.First onClick={() => setCurrentPage(1)} disabled={currentPage === 1} />
+                            <Pagination.Prev onClick={handlePreviousPage} disabled={currentPage === 1} />
+
+
+                           
+
+                            {Array.from({ length: totalPages }, (_, i) => i + 1).map((pageNumber) => (
+                                <Pagination.Item
+                                    key={pageNumber}
+                                    active={pageNumber === currentPage}
+                                    onClick={() => handlePageChange(pageNumber)}
+                                >
+                                    {pageNumber}
+                                </Pagination.Item>
+                            ))}
+
+                                
+                            
+                            
+                            <Pagination.Next onClick={handleNextPage} disabled={currentPage === totalPages} />
+                            <Pagination.Last onClick={() => setCurrentPage(totalPages)} disabled={currentPage === totalPages} />
+                        </Pagination>
                     </Container>
                 )}
+                
             </Container>
         )
-}
+    }
 
 export default SearchBar
